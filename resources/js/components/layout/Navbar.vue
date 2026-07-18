84815ae23ecabfefae01f86415eb562f2c5b21b7
unknown <sabbihkyau@gmail.com>
2026-07-18 21:08:10 +0600

    navbar for desktop with tailwind css

--- resources/js/components/layout/Navbar.vue
+++ resources/js/components/layout/Navbar.vue
@@ -1,16 +1,16 @@
 <template>
-    <header class="navbar">
-        <div class="container">
+    <header class="fixed top-0 left-0 right-0 z-50 py-5">
+        <div class="mx-auto flex max-w-7xl items-center justify-between rounded-full px-6 py-4">
             <!-- logo -->
-            <RouterLink to="/" class="logo">Doctor Diary</RouterLink>
+            <RouterLink to="/" class="text-2xl font-bold traking-tight">Doctor Diary</RouterLink>
             <!-- menus -->
-            <nav class="nav-links">
-                <RouterLink v-for="item in navigationItems" :key="item.to" :to="item.to">
+            <nav class="hidden items-center gap-8 lg:flex">
+                <RouterLink v-for="item in navigationItems" :key="item.to" :to="item.to" class="text-sm font-medium transition-colors duration-200 hover:text-blue-600">
                     {{ item.name }}
                 </RouterLink>
             </nav>
             <!-- CTA -->
-            <RouterLink to="/login" class="btn btn-primary">Get Started</RouterLink>
+            <RouterLink to="/login" class="rounded-full bg-blue-600 px-5 py-3 text-sm font-semibold text-white transition hover: bg-blue-700">Get Started</RouterLink>
         </div>
     </header>
 </template>
