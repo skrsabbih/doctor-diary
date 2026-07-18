fa54d6b940f8694511503025e0f0207715d3c1c2
unknown <sabbihkyau@gmail.com>
2026-07-18 20:30:47 +0600

    vue items with v-for iteration

--- resources/js/components/layout/Navbar.vue
+++ resources/js/components/layout/Navbar.vue
@@ -2,17 +2,40 @@
     <header class="navbar">
         <div class="container">
             <!-- logo -->
-            <RouterLink to="/" class="logo">Doctor Daily</RouterLink>
+            <RouterLink to="/" class="logo">Doctor Diary</RouterLink>
             <!-- menus -->
             <nav class="nav-links">
-                <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/about">About</RouterLink>
-                <RouterLink to="/journey">Journey</RouterLink>
-                <RouterLink to="/blogs">Blogs</RouterLink>
-                <RouterLink to="/contact">Contact</RouterLink>
+                <RouterLink v-for="item in navigationItems" :key="item.to" :to="item.to">
+                    {{ item.name }}
+                </RouterLink>
             </nav>
             <!-- CTA -->
             <RouterLink to="/login" class="btn btn-primary">Get Started</RouterLink>
         </div>
     </header>
 </template>
+
+<script setup>
+const navigationItems = [
+    {
+        name: 'Home',
+        to: '/',
+    },
+    {
+        name: 'About',
+        to: '/about',
+    },
+    {
+        name: 'Journey',
+        to: '/journey',
+    },
+    {
+        name: 'Blogs',
+        to: '/blogs',
+    },
+    {
+        name: 'Contact',
+        to: '/contact',
+    },
+]
+</script>
